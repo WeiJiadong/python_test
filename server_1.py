b0f6a4c737c244e867e48250d80e6bb0fcf1a536
vivid <vivid@localhost.localdomain>
2016-06-19 04:22:39 +0800

    test1

--- server_1.py
+++ server_1.py
@@ -1,4 +1,4 @@
-# coding="utf8"
+# coding=utf8
 
 import socket
 
@@ -8,7 +8,6 @@ listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
-
 print "Serving HTTP on port %s ..." % PORT
 
 while True:
@@ -16,6 +15,10 @@ while True:
     request = client_connection.recv(1024)
     print request
 
-    http_response = "HTTP/1.1 200 OK\r\n\r\nHello,World!\r\n"
+    http_response = """\
+    HTTP/1.1 200 OK
+
+    Hello,World!
+    """
     client_connection.sendall(http_response)
     client_connection.close()
